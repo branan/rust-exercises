a179e59e3b4d3bf367dc927e3b125caf711496f9
Branan Riley <branan@puppet.com>
2018-04-25 11:48:27 -0700

    Use zero-copy implementation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
+extern crate failure;
 extern crate serde_json;
 
 use std::collections::HashMap;
 use std::env;
 use std::fs::File;
+use std::io::Read;
 
-type Counts = HashMap<String, i32>;
-type JsonHash = HashMap<String, HashMap<String, String>>;
+use failure::{err_msg, Error};
 
-fn aggregate(data: JsonHash) -> HashMap<String, Counts> {
+type Counts<'a> = HashMap<&'a str, i32>;
+type JsonHash<'a> = HashMap<&'a str, HashMap<&'a str, &'a str>>;
+
+fn aggregate<'a> (data: &'a JsonHash) -> HashMap<&'a str, Counts<'a>> {
     let mut agg = HashMap::new();
     for (_, val) in data {
         for (k, v) in val {
-            let vals = agg.entry(k).or_insert(Counts::new());
+            let vals = agg.entry(*k).or_insert(Counts::new());
             let nodes = vals.entry(v).or_insert(0);
             *nodes += 1
         }
@@ -19,20 +23,21 @@ fn aggregate(data: JsonHash) -> HashMap<String, Counts> {
     agg
 }
 
-fn main() {
+fn inner_main() -> Result<(), Error> {
     let args: Vec<_> = env::args().collect();
-    let inputfile = if let Some(x) = args.get(1) { x } else {
-        println!("Usage: collections <inputfile>");
-        std::process::exit(1)
-    };
+    let inputfile = args.get(1).ok_or(err_msg("Usage: collections <inputfile>"))?;
 
-    let file = File::open(inputfile).unwrap();
-    let data: JsonHash = match serde_json::from_reader(file) {
-        Ok(d) => d,
-        Err(e) => { println!("{}", e); std::process::exit(1) }
-    };
-    let agg = aggregate(data);
-    println!("{}", serde_json::to_string_pretty(&agg).unwrap())
+    let mut input_string = String::new();
+    File::open(inputfile)?.read_to_string(&mut input_string)?;
+    let data = serde_json::from_str(&input_string)?;
+    let agg = aggregate(&data);
+    println!("{}", serde_json::to_string_pretty(&agg)?);
+
+    Ok(())
+}
+
+fn main() {
+    inner_main().unwrap();
 }
 
 #[cfg(test)]
@@ -42,47 +47,47 @@ mod tests {
     #[test]
     fn aggregate_works() {
         let data = hashmap!{
-            String::from("node1") => hashmap!{
-                String::from("application") => String::from("forge"),
-                String::from("version") => String::from("10.0"),
-                String::from("role") => String::from("app-server"),
+            "node1" => hashmap!{
+                "application" => "forge",
+                "version" => "10.0",
+                "role" => "app-server",
             },
-            String::from("node2") => hashmap!{
-                String::from("application") => String::from("forge"),
-                String::from("version") => String::from("10.0"),
-                String::from("role") => String::from("util"),
-                String::from("location") => String::from("slice"),
+            "node2" => hashmap!{
+                "application" => "forge",
+                "version" => "10.0",
+                "role" => "util",
+                "location" => "slice",
             },
-            String::from("node3") => hashmap!{
-                String::from("application") => String::from("forge"),
-                String::from("version") => String::from("9.5"),
-                String::from("role") => String::from("db"),
+            "node3" => hashmap!{
+                "application" => "forge",
+                "version" => "9.5",
+                "role" => "db",
             },
-            String::from("node4") => hashmap!{
-                String::from("application") => String::from("anubis"),
-                String::from("location") => String::from("slice"),
-                String::from("role") => String::from("worker"),
+            "node4" => hashmap!{
+                "application" => "anubis",
+                "location" => "slice",
+                "role" => "worker",
             },
         };
 
-        let agg = ::aggregate(data);
+        let agg = ::aggregate(&data);
         assert_eq!(agg, hashmap!{
-            String::from("application") => hashmap!{
-                String::from("forge") => 3,
-                String::from("anubis") => 1,
+            "application" => hashmap!{
+                "forge" => 3,
+                "anubis" => 1,
             },
-            String::from("version") => hashmap!{
-                String::from("10.0") => 2,
-                String::from("9.5") => 1,
+            "version" => hashmap!{
+                "10.0" => 2,
+                "9.5" => 1,
             },
-            String::from("role") => hashmap!{
-                String::from("app-server") => 1,
-                String::from("util") => 1,
-                String::from("db") => 1,
-                String::from("worker") => 1,
+            "role" => hashmap!{
+                "app-server" => 1,
+                "util" => 1,
+                "db" => 1,
+                "worker" => 1,
             },
-            String::from("location") => hashmap!{
-                String::from("slice") => 2,
+            "location" => hashmap!{
+                "slice" => 2,
             }
         });
     }
